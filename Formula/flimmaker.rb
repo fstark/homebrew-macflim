f635926a91abe009f87a6dfa1ab995322b3828b6
Frederic Stark <frederic.stark@gmail.com>
2025-11-23 18:58:02 +0100

    Add SHA256 hashes for v3.0.0 release

--- Formula/flimmaker.rb
+++ Formula/flimmaker.rb
@@ -6,10 +6,10 @@ class Flimmaker < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/fstark/macflim/releases/download/v3.0.0/flimmaker-arm64-macos.tar.gz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "3ed1e06a602ef01bf75f128f7e05299af408e7ba6153f70e9208d5832dc08c1a"
     elsif Hardware::CPU.intel?
       url "https://github.com/fstark/macflim/releases/download/v3.0.0/flimmaker-x86_64-macos.tar.gz"
-      sha256 "PLACEHOLDER_X86_64_SHA256"
+      sha256 "82e927bbfa01a755777d394efc7983a07b306fea0e9602ab8ff3b46ac6a4e0b4"
     end
   end
 
