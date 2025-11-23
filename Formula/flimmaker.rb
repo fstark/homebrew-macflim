a117477a4b99449877163ca618469661c7af9350
Frederic Stark <frederic.stark@gmail.com>
2025-11-23 20:55:33 +0100

    Switch to building from source - solves code signing issues

--- Formula/flimmaker.rb
+++ Formula/flimmaker.rb
@@ -1,25 +1,24 @@
 class Flimmaker < Formula
   desc "Create classic Macintosh FLIM movies from modern video files"
   homepage "https://github.com/fstark/macflim"
+  url "https://github.com/fstark/macflim/archive/refs/tags/v3.0.1.tar.gz"
+  sha256 "b2bde14e89967596becf06f509b1f4b6ded1c75e1a35265405c04f750f9628d7"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/fstark/macflim/releases/download/v3.0.1/flimmaker-arm64-macos.tar.gz"
-      sha256 "7f9cebb23817fad9b7e58a825c5a4bba65c006fb3d3baa124e94ebfed8c59d3c"
-    elsif Hardware::CPU.intel?
-      url "https://github.com/fstark/macflim/releases/download/v3.0.1/flimmaker-x86_64-macos.tar.gz"
-      sha256 "8aae345cb22dc3b0cdfc154f74e938502bbb8817448d0e12f3030d8bc2bb99ae"
-    end
-  end
+  depends_on "gcc@14" => :build
+  depends_on "ffmpeg"
+  depends_on "imagemagick"
+  depends_on "yt-dlp"
 
   def install
-    # Install everything to libexec to keep libraries bundled
-    libexec.install "flimmaker", "lib"
-    
-    # Create wrapper script in bin that calls the binary in libexec
-    # The binary's @rpath is set to @executable_path/lib, so it will find libraries correctly
-    (bin/"flimmaker").write_env_script libexec/"flimmaker", {}
+    # Build with g++-14
+    cd "src" do
+      system "make", "clean"
+      system "make", "release", "CPP=g++-14"
+    end
+
+    # Install the binary
+    bin.install "flimmaker"
   end
 
   test do
