3a50dbb94462c5cc8e2eb7fb9d1370a6d4b077a0
Frederic Stark <frederic.stark@gmail.com>
2025-11-23 19:04:35 +0100

    Update to v3.0.1

--- Formula/flimmaker.rb
+++ Formula/flimmaker.rb
@@ -5,11 +5,11 @@ class Flimmaker < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/fstark/macflim/releases/download/v3.0.0/flimmaker-arm64-macos.tar.gz"
-      sha256 "3ed1e06a602ef01bf75f128f7e05299af408e7ba6153f70e9208d5832dc08c1a"
+      url "https://github.com/fstark/macflim/releases/download/v3.0.1/flimmaker-arm64-macos.tar.gz"
+      sha256 "7f9cebb23817fad9b7e58a825c5a4bba65c006fb3d3baa124e94ebfed8c59d3c"
     elsif Hardware::CPU.intel?
-      url "https://github.com/fstark/macflim/releases/download/v3.0.0/flimmaker-x86_64-macos.tar.gz"
-      sha256 "82e927bbfa01a755777d394efc7983a07b306fea0e9602ab8ff3b46ac6a4e0b4"
+      url "https://github.com/fstark/macflim/releases/download/v3.0.1/flimmaker-x86_64-macos.tar.gz"
+      sha256 "8aae345cb22dc3b0cdfc154f74e938502bbb8817448d0e12f3030d8bc2bb99ae"
     end
   end
 
